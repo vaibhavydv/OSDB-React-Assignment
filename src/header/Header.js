3a27a9c0e450895e581865043518da81d9013906
VAIBHAV YADAV <vaibhavyadav@surmountsoft.in>
2019-07-01 19:32:36 +0530

    routing and bootstrap

---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './headerStyle.js';
+
+function Header() {
+    return(
+        <div className="customHeader">
+        <h1>OSDB</h1>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import pageOne from './page1';
+import pageTwo from './page2';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/page-one">Page 1</Link>
+        </li>
+        <li>
+          <Link to="/page-two">page 2</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/page-one" component={pageOne} />
+      <Route path="/page-two" component={pageTwo} />
+    </div>
+    </Router>
+)
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
