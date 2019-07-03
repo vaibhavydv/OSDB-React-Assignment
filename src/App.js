59a37dec456bbf25c40683f739b68ebd14b161d3
VAIBHAV YADAV <vaibhavyadav@surmountsoft.in>
2019-07-03 19:35:41 +0530

    with menu

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './header/Header';
 
 function App() {
   return (
-    <div className="customHeader">
-        <h1>OSDB</h1>
-      </div>
-     
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Launch App
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="/header"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Click Here
-    //     </a>
-    //   </header>
-    // </div>
+    <>
+    <Header/>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import pageOne from './page1';
 import pageTwo from './page2';
 import * as serviceWorker from './serviceWorker';
 
+// routes 
 const routing = (
     <Router>
-        <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/page-one">Page 1</Link>
-        </li>
-        <li>
-          <Link to="/page-two">page 2</Link>
-        </li>
-      </ul>
       <Route exact path="/" component={App} />
       <Route path="/page-one" component={pageOne} />
       <Route path="/page-two" component={pageTwo} />
-    </div>
     </Router>
 )
-//ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(routing, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
