6062e4166dfa2efce5ae9fc30e0edf6f2ba9afc0
Vaibhav Yadav <imvaibhavyadav@gmail.com>
2019-07-07 18:17:35 +0530

    fix

---
+++ src/homepage/trending-news/trendingNews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './tnStyle.css';
+
+function NewsOsdb() {
+    return (
+        <div className="newsclass">
+            <h2>TRENDING NEWS</h2>
+        </div>
+    );
+}
+export default NewsOsdb; 
