ed3d0caceac974d27b2dd020b3b9062a353278c6
Vaibhav Yadav <imvaibhavyadav@gmail.com>
2019-07-07 16:16:11 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-//import Header from './header/Header';
-//import BreakingNews from './homepage/breaking-news/breakingNews';
-//import TrendingPlayers from './homepage/trending-players/trendingPlayers';
-//import FactOfTheDay from './homepage/fact-otd/factOfTheDay';
+import Header from './header/Header';
+import BreakingNews from './homepage/breaking-news/breakingNews';
+import TrendingPlayers from './homepage/trending-players/trendingPlayers';
+import FactOfTheDay from './homepage/fact-otd/factOfTheDay';
 import Footer from './osdbFooter/footer';
 function App() {
   return (
     <>
-    {/* <Header/>
+    <Header/>
     <BreakingNews/>
     <TrendingPlayers />
-    <FactOfTheDay /> */}
+    <FactOfTheDay />
     <Footer />
     </>
   );
