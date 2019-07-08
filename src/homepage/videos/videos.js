2efe663e13dc84f21d70c0a0cef45d1118555905
Vaibhav Yadav <imvaibhavyadav@gmail.com>
2019-07-09 01:16:59 +0530

    Videos section done

--- src/homepage/videos/videos.js
+++ src/homepage/videos/videos.js
@@ -1,28 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import VideoMain from '../../assets/images/video_m.png'
 import VideoLeft from '../../assets/images/video_l.png'
 import VideoRight from '../../assets/images/video_r.png'
+import PlayVideo from '../../assets/images/video_play.svg'
 import './videosStyle.css';
 
 function HomeVideos() {
     return (
         <>
-        <h1 className="videoHead">VIDEOS</h1>  
         <div className="strip"></div>
-        <div className="videosMain">
-        <div className="videoList">
-            <div className="leftButton">&larr;</div>
-            <div className="rightButton">&rarr;</div>
+        <div className="leftButton">&larr;</div>
+        <div className="rightButton">&rarr;</div>
+        <div className="playButton"><img src={PlayVideo}/></div>
+        <h1 className="videoHead">VIDEOS</h1>  
+        <div>
             <div className="left" >
-                <img src={VideoLeft} alt="OSDB"/>
+                <img src={VideoLeft}/>
             </div>
-            <img src={VideoMain} alt="OSDB"/>
+            <div className="videoMain">
+                <div>
+                        <img src={VideoMain} alt="OSDB" />
+                </div>
+                    <div className="vdoCover">
+                        <h1>Sania Mirza give India one more medal</h1>
+                        <div className="vdoInfo">
+                            <p>1:14 pm&nbsp;</p><p>&bull;&nbsp;27 Feb 2018&nbsp;</p><p>&bull;&nbsp;By: Vaibhav</p>
+                            <ul>
+                                <li><Link to="https://facebook.com/vaibhavydv"><i class="fa fa-facebook"></i></Link></li>
+                                <li><Link to=""><i class="fa fa-twitter"></i></Link></li>  
+                                <li><Link to=""><i class="fa fa-share-alt"></i></Link></li>    
+                            </ul>
+                        </div>
+                    </div>
+            </div>
             <div className="right">
-                <img src={VideoRight} alt="OSDB"/>  
+                <img src={VideoRight}/>
             </div>
-           
-            <img/>
-        </div>
         </div>
         </>
            
