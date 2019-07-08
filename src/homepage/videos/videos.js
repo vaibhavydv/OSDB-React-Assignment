01aab805825e9c1f2432f98807cab749432c0d56
VAIBHAV YADAV <vaibhavyadav@surmountsoft.in>
2019-07-08 19:45:07 +0530

    >

--- src/homepage/videos/videos.js
+++ src/homepage/videos/videos.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import VideoMain from '../../assets/images/video_m.png'
+import VideoLeft from '../../assets/images/video_l.png'
+import VideoRight from '../../assets/images/video_r.png'
 import './videosStyle.css';
 
 function HomeVideos() {
     return (
         <>
         <h1 className="videoHead">VIDEOS</h1>  
-        <div className="videosMain">
         <div className="strip"></div>
+        <div className="videosMain">
         <div className="videoList">
-        <div className="leftButton">&larr;</div>
-        <div className="rightButton">&rarr;</div>
-            <img src={VideoMain} alt="OSDB"></img>
+            <div className="leftButton">&larr;</div>
+            <div className="rightButton">&rarr;</div>
+            <div className="left" >
+                <img src={VideoLeft} alt="OSDB"/>
+            </div>
+            <img src={VideoMain} alt="OSDB"/>
+            <div className="right">
+                <img src={VideoRight} alt="OSDB"/>  
+            </div>
+           
+            <img/>
         </div>
         </div>
         </>
